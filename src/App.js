1281f150d148d8b0a5de31538d0db61acab46b15
kyang9898 <kyang0908@gmail.com>
2022-04-18 14:50:51 -0400

    fixed formatting

--- src/App.js
+++ src/App.js
@@ -31,18 +31,18 @@ const Total = (props) => {
 
 const App = () => {
   const course = {
-    name: 'Half Stack application development',
+    name: 'Half Stack application development ',
     parts: [
       {
-        name: 'Fundamentals of React',
+        name: 'Fundamentals of React ',
         exercises: 10
       },
       {
-        name: 'Using props to pass data',
+        name: 'Using props to pass data ',
         exercises: 7
       },
       {
-        name: 'State of a component',
+        name: 'State of a component ',
         exercises: 14
       }
     ]
